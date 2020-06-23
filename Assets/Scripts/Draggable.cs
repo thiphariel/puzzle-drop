d43e42110119324f25e63b92fac634259484cb3e
Thiphariel <thiphariel@gmail.com>
2020-06-23 13:00:11 +0200

    initial commit

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    public Transform destination;
+    public Vector2 initialPosition;
+    public bool locked;
+
+    private Collider2D current;
+    private float dX, dY; // Delta offset from touch to transform position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.touches[0];
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    // Retrieve the offset position from touch to transform object
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
+                    {
+                        dX = touchPosition.x - transform.position.x;
+                        dY = touchPosition.y - transform.position.y;
+                        current = GetComponent<Collider2D>();
+                    } else
+                    {
+                        current = null;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (current)
+                    {
+                        current.GetComponent<Transform>().transform.position = new Vector2(touchPosition.x - dX, touchPosition.y - dY);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - destination.position.x) <= 2f && Mathf.Abs(transform.position.y - destination.position.y) <= 2f)
+                    {
+                        transform.position = destination.position;
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = initialPosition;
+                        current = null;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Draggable[] draggables;
+    public GameObject win;
+    public GameObject menu;
+
+    private List<Vector2> m_positions = new List<Vector2> {
+        new Vector2(-4, -2.5f),
+        new Vector2(0, -2.5f),
+        new Vector2(4, -2.5f)
+    };
+
+    private Vector2 GetRandomPosition()
+    {
+        Vector2 position = m_positions[Random.Range(0, m_positions.Count)];
+        m_positions.Remove(position);
+        return position;
+    }
+
+    private float counter = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        win.SetActive(false);
+
+        for (int i = 0; i < draggables.Length; i++)
+        {
+            draggables[i].transform.position = GetRandomPosition();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hasWin = true;
+
+        foreach (Draggable draggable in draggables)
+        {
+            if (!draggable.locked)
+            {
+                hasWin = false;
+                continue;
+            }
+        }
+
+        if (hasWin)
+        {
+            win.SetActive(true);
+
+            if (counter >= 0f)
+            {
+                counter -= Time.deltaTime;
+
+                if (counter <= 0f)
+                {
+                    Reset();
+                }
+            }
+        }
+    }
+
+    public void OpenMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+    }
+
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    private void Reset()
+    {
+        win.SetActive(false);
+        counter = 3f;
+
+        m_positions = new List<Vector2> {
+            new Vector2(-4, -2.5f),
+            new Vector2(0, -2.5f),
+            new Vector2(4, -2.5f)
+        };
+
+        foreach (Draggable draggable in draggables)
+        {
+            draggable.locked = false;
+            Vector2 position = GetRandomPosition();
+            draggable.initialPosition = position;
+            draggable.transform.position = position;
+        }
+    }
+}
